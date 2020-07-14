af3a4b31d05aa8c2813306909955305cb22ebcf2
Mohammed AouladBouchta <java.mohammed@gmail.com>
2020-07-14 12:20:32 +0100

    add build folder

---
+++ build/precache-manifest.50b64726b2fd50a6df39460d00740f0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a653927e9a865535e3b7a00765f64d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db779ea087f75c1b5e12",
+    "url": "/static/js/2.ba68c337.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.ba68c337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81b2b600d3d2c4210a6f",
+    "url": "/static/js/main.f8d06d86.chunk.js"
+  },
+  {
+    "revision": "fc017e358560382f3dd2",
+    "url": "/static/js/runtime-main.94bcb1b5.js"
+  }
+]);
